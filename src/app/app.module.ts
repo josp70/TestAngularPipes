d4d3a6ab675c506b3201d52c4760cc3dd99685ad
Jorge Pérez <josp.jorge@gmail.com>
2018-03-10 12:23:01 +0100

    using NgMathPipesModule
    if you build with `ng build --aot` or `ng build --prod` you may receive:
    
    ```
    ERROR in : Unexpected value 'NgMathPipesModule in
    TestAngularPipes/node_modules/angular-pipes/angular-pipes.d.ts'
    imported by the module 'AppModule in TestAngularPipes/src/app/app.module.ts'.
    Please add a @NgModule annotation.
    ```

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { NgMathPipesModule } from 'angular-pipes';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgMathPipesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
